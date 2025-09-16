ce038138c35673a80426092e41c0def628940487
Aranya Kishor Das <aranya.akd@gmail.com>
2025-09-17 00:39:07 +0600

    feat: add SidebarAgent and SidebarWarehouse components with navigation links

---
+++ include/SidebarAgent.php
@@ -0,0 +1,122 @@
+<?php
+// session_start();
+?>
+<!DOCTYPE html>
+<!-- Created by CodingLab |www.youtube.com/CodingLabYT-->
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sidebar</title>
+    <link rel="stylesheet" href="sidebar.css">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body class="bg-custom">
+    <div class="sidebar pl-5 m-0">
+        <div class="logo-details">
+            <i class='bx bx-menu' id="btn"></i>
+        </div>
+        <ul class="nav-list p-0">
+            <li>
+                <i class='bx bx-search'></i>
+                <input type="text" placeholder="Search...">
+                <span class="tooltip">Search</span>
+            </li>
+            <li>
+                <a href="..\Dashboard\Dashboard.php">
+                    <i class='bx bx-grid-alt'></i>
+                    <span class="links_name">Dashboard</span>
+                </a>
+                <span class="tooltip">Dashboard</span>
+            </li>
+            <li>
+                <a href="..\Investment Page\Investment.php">
+                    <i class='bx bx-store-alt'></i>
+                    <span class="links_name">Invest in Store</span>
+                </a>
+                <span class="tooltip">Invest in Store</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-target-lock'></i>
+                    <span class="links_name">Projection</span>
+                </a>
+                <span class="tooltip">Projection</span>
+            </li>
+            <li>
+                <a href="..\Receipt Page\RPage.php">
+                    <i class='bx bx-receipt'></i>
+                    <span class="links_name">Receipt</span>
+                </a>
+                <span class="tooltip">Receipt</span>
+            </li>
+            <li>
+                <a href="..\Stock and Storage\Stock.php">
+                    <i class='bx bx-archive'></i>
+                    <span class="links_name">Track Storage</span>
+                </a>
+                <span class="tooltip">Track Storage</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-cart-alt'></i>
+                    <span class="links_name">Order</span>
+                </a>
+                <span class="tooltip">Order</span>
+            </li>
+            <li>
+                <a href="#">
+                    <i class='bx bx-heart'></i>
+                    <span class="links_name">Saved</span>
+                </a>
+                <span class="tooltip">Saved</span>
+            </li>
+            <li>
+                <a href="..\User Settings\Settings.php">
+                    <i class='bx bx-cog'></i>
+                    <span class="links_name">Setting</span>
+                </a>
+                <span class="tooltip">Setting</span>
+            </li>
+            <li class="profile">
+                <div class="profile-details">
+                    <img src="https://www.svgrepo.com/show/23012/profile-user.svg" alt="profileImg">
+                    <div class="name_job">
+                        <div class="name">Aranya</div>
+                    </div>
+                </div>
+                <i class='bx bx-log-out' id="log_out"></i>
+            </li>
+        </ul>
+    </div>
+    <section class="home-section">
+    </section>
+    <script>
+        let sidebar = document.querySelector(".sidebar");
+        let closeBtn = document.querySelector("#btn");
+        let searchBtn = document.querySelector(".bx-search");
+
+        closeBtn.addEventListener("click", () => {
+            sidebar.classList.toggle("open");
+            menuBtnChange(); //calling the function(optional)
+        });
+
+        searchBtn.addEventListener("click", () => { // Sidebar open when you click on the search iocn
+            sidebar.classList.toggle("open");
+            menuBtnChange(); //calling the function(optional)
+        });
+
+        // following are the code to change sidebar button(optional)
+        function menuBtnChange() {
+            if (sidebar.classList.contains("open")) {
+                closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
+            } else {
+                closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
+            }
+        }
+    </script>
+</body>
+
+</html>
