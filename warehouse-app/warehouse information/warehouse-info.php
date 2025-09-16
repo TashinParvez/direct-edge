9b541d0df5944fd75bcf1d85a3f0a0628653393b
Nomanrifat05 <noman.rifat.3@gmail.com>
2025-09-17 04:20:26 +0600

    update warehouse info

--- warehouse-app/warehouse information/warehouse-info.php
+++ warehouse-app/warehouse information/warehouse-info.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Warehouse Info</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../warehouse information/warehouse-info.css">
 </head>
 <body>
+     <?php //include '../../include/Sidebar.php'; 
+    ?>
     <div class="container">
         <h1>Warehouse Information</h1>
 
@@ -222,6 +224,6 @@
         </div>
     </div>
 
-    <script src="script.js"></script>
+    <script src="../warehouse information/warehouse-info.js"></script>
 </body>
 </html>
