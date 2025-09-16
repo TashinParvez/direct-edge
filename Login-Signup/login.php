886a7ff342335e0ba062a191c645ea41719d08c9
Mahbub <mrahman623a@gmail.com>
2025-09-16 20:53:56 +0600

    feat: add favicon

--- Login-Signup/login.php
+++ Login-Signup/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In DirectEdge</title>
-    <link rel="icon" type="image/x-icon" href="../Logo/LogoBG.png">
+    <link rel="icon" type="image/x-icon" href="../Logo/Favicon.png">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
